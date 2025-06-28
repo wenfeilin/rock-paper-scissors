00da212b10d6fa03ae52e430473d3d71f549e6de
Wenfei Lin <wl688728@gmail.com>
2025-06-28 18:07:41 -0400

    Implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+  const randomNum = Math.floor((Math.random() * 3)) + 1;
+  let computerChoice = "";
+
+  switch (randomNum) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "scissors";
+      break;
+    case 3:
+      computerChoice = "paper";
+      break;
+  }
+
+  return computerChoice;
+}
+
+// function getHumanChoice() {
+  
+// }
