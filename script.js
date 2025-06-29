02d3d847e59ea00a2e60e361f7e6fd8f94ad07f8
Wenfei Lin <wl688728@gmail.com>
2025-06-29 01:05:15 -0400

    Implement playGame and finish ending game logic

--- script.js
+++ script.js
@@ -18,45 +18,87 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Pick rock, paper, or scissors.");
+  return prompt("Pick rock, paper, or scissors.").toLowerCase();
 }
 
 function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-function playRound(humanChoice, computerChoice) {
-  const humanChoiceLowerCase = humanChoice.toLowerCase();
-  
-  // User draws with computer.
-  if (humanChoiceLowerCase === computerChoice) {
-    console.log(`It's a draw! You and the computer both chose ${computerChoice}.`);
+function printScore(humanScore, computerScore) {
+  console.log(`You: ${humanScore}`);
+  console.log(`Computer: ${computerScore}`);
+}
+
+function determineWinner(humanScore, computerScore) {
+  if (humanScore === computerScore) {
+    return "draw";
   } else {
-    // User win scenarios:
-    if ((humanChoiceLowerCase === "rock") && (computerChoice === "scissors") ||
-        (humanChoiceLowerCase === "scissors") && (computerChoice === "paper") ||
-        (humanChoiceLowerCase === "paper") && (computerChoice === "rock") ) {
-      console.log(`You win! ${capitalize(humanChoiceLowerCase)} beats ${computerChoice}.`);
-      humanScore++;
-    } else {
-      // User loses.
-      console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoiceLowerCase}.`);
-      computerScore++;
-    }
+    return humanScore > computerScore ? "human" : "computer";
   }
 }
 
-/* Total Scores */
-let humanScore = 0;
-let computerScore = 0;
+function printWinner(winner, numRounds) {
+  const openingMsg = `After ${numRounds} rounds of intense rock, paper, and scissors between you \
+                      and the computer,`;
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+  if (winner === "draw") {
+    console.log(`${openingMsg} it's a draw.`);
+    return;
+  }
 
-playRound(humanChoice, computerChoice);
+  if (winner === "human") {
+    console.log(`${openingMsg} you have come out on top! Congratulations!`);
+  } else {
+    console.log(`${openingMsg} you have been defeated by the computer. Better luck next time!`);
+  }
+}
 
-/* 
-For testing:
-console.log(`Human: ${humanScore}`);
-console.log(`Computer: ${computerScore}`);
-*/
+function printFinalScores(humanScore, computerScore) {
+  console.log("===== Final Scores =====");
+  printScore(humanScore, computerScore);
+  console.log("========================");
+}
+
+function playGame() {
+  function playRound(humanChoice, computerChoice) {
+    // User draws with computer.
+    if (humanChoice === computerChoice) {
+      console.log(`It's a draw! You and the computer both chose ${computerChoice}.`);
+      return;
+    }
+
+    // User win scenarios: rock beats scissors, scissors beats paper, paper beats rock.
+    if ((humanChoice === "rock") && (computerChoice === "scissors") ||
+        (humanChoice === "scissors") && (computerChoice === "paper") ||
+        (humanChoice === "paper") && (computerChoice === "rock") ) {
+      console.log(`You win! ${capitalize(humanChoice)} beats ${computerChoice}.`);
+      humanScore++;
+    } else {
+      // User loses.
+      console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`);
+      computerScore++;
+    }
+  }
+  
+  /* Total Scores */
+  let humanScore = 0;
+  let computerScore = 0;
+  
+  const rounds = 5;
+
+  // Play for 5 rounds.
+  for (let i = 0; i < rounds; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    printScore(humanScore, computerScore);
+  }
+
+  // Declare the winner and display final scores!
+  const winner = determineWinner(humanScore, computerScore);
+  printWinner(winner, rounds);
+  printFinalScores(humanScore, computerScore);
+}
+
+playGame();
